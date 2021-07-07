d56e56d1a0ac2d887c183b34b59affefd67e0eb9
Cephas Chapa <cephaschapa@gmail.com>
2021-07-07 14:52:14 +0200

    Background change for Navitems

--- components/Navitems.js
+++ components/Navitems.js
@@ -4,10 +4,10 @@ function Navitems({name, counter, icon}) {
 
     return (
         <div onClick={function() {
-           }} className={`flex cursor-pointer p-2 items-center mb-2 rounded-xl transition duration-150 transform hover:bg-gray-50 hover:text-[#198A00]`}>
+           }} className={`${counter < 0 ? 'bg-gray-50 text-[#198A00]' : 'bg-none text-white'} flex cursor-pointer p-2 items-center mb-2 rounded-xl transition duration-150 transform hover:bg-gray-50 hover:text-[#198A00]`}>
             <p className="mr-2">{icon}</p>
             <p>{name}</p>
-            <p className={`bg-white'  'bg-none'  ml-32 p-2 rounded-xl text-[#198A00] right-1 absolute text-xs font-bold flex items-center`}>{counter}</p>
+            <p className={`${counter > 0 ? 'bg-white' : 'bg-none' } ml-32 p-2 rounded-xl text-[#198A00] right-1 absolute text-xs font-bold flex items-center`}>{counter}</p>
         </div>
     )
 }
