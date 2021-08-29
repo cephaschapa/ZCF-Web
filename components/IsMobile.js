c70fab25bf73f79bd2da9f8c67d4adf6fe22b0e1
cephaschapa <cephaschapa@gmail.com>
2021-08-29 23:15:04 +0200

    Pushed

--- components/IsMobile.js
+++ components/IsMobile.js
@@ -2,7 +2,7 @@ import { ExclamationIcon } from '@heroicons/react/outline'
 function IsMobile() {
     return (
         <div className="flex items-center h-screen bg-[#198A00] p-2">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col p-2 h-screen bg-[#198A00] items-center">
             <ExclamationIcon className="h-16 w-16 text-white m-2"/>
             <p className="flex text-white text-center">You are using a mobile device. This website is only optimised for desktop devices.</p>
             </div>
